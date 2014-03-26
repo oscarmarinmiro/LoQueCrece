85b6df7c66ee1ec3d70a25a8fb6b7e92f78c8317
oscarmarinmiro <oscarmarinmiro@github.com>
2014-03-26 11:19:28 +0100

    -

--- R_sessions/March2014/components.R
+++ R_sessions/March2014/components.R
@@ -3,12 +3,16 @@
 
 # http://www.statmethods.net/advstats/factor.html
 
-myData = read.csv("PentagonGrowthClassificationPlus.csv")
+myData = read.csv("PGC_V2.csv")
 
-myData = myData[,c(2,3,4,5,6,12)]
+initData = myData
+
+myData = myData[myData$Wave==3,c(2,3,4,5,6,9)]
 
 #results <- princomp(myData, corr=TRUE,na.action=na.omit)
 results <- princomp(~.,data=myData, cor = TRUE, na.action=na.omit)
+
+biplot(results)
 
 summary(results)
 loadings(results)
--- R_sessions/March2014/corrgram.R
+++ R_sessions/March2014/corrgram.R
@@ -2,21 +2,23 @@ library(corrgram)
 
 # load nice data and impactpattern, and tags (excluding CrowdSourced)
 
-myData = read.csv("PentagonGrowthClassificationPlus.csv")
+myData = read.csv("PGC_V2.csv")
 
 
 corrData = myData[,c(2:12)]
-corrData = myData
 # corrData
 
 corrMatrix = cor(corrData, use="complete.obs", method="kendall")
 #corrMatrix
 
 # .conf .shade .pie .pts .density (?) .ellipse .txt ... diag.panel=panel.density ==> Distribucion de la variable
-corrgram(corrData,lower.panel=panel.shade,upper.panel=panel.conf, diag.panel=panel.density,cex=0.7, main="blah")
 
-#pdf(file="corr.justTags.pdf", width=10, height=10)
+pdf(file="PentaWaveAlphas.all.pdf", width=10, height=10)
+
+
+corrgram(corrData,lower.panel=panel.shade,upper.panel=panel.conf, diag.panel=panel.density,cex=0.7, main="Pentagon-Wave-Alphas for all")
+
 
 #corrgram(corrData,lower.panel=panel.shade,upper.panel=panel.conf, diag.panel=panel.density,cex=0.7, main="correlogram for new tags (29Nov2013)",na.rm= TRUE)
 
-#dev.off()
+dev.off()
